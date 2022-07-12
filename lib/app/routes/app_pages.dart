f2d58a4670ed96439328b44865a4ba07bf118afb
mrezkys <mrezkysulihin@gmail.com>
2022-07-12 16:51:17 +0800

    feat: update pages and routes

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -2,10 +2,20 @@ import 'package:get/get.dart';
 
 import 'package:game_islami/app/modules/home/bindings/home_binding.dart';
 import 'package:game_islami/app/modules/home/views/home_view.dart';
+import 'package:game_islami/app/modules/pilih_surah/bindings/pilih_surah_binding.dart';
+import 'package:game_islami/app/modules/pilih_surah/views/pilih_surah_view.dart';
 import 'package:game_islami/app/modules/profile/bindings/profile_binding.dart';
 import 'package:game_islami/app/modules/profile/views/profile_view.dart';
 import 'package:game_islami/app/modules/rank/bindings/rank_binding.dart';
 import 'package:game_islami/app/modules/rank/views/rank_view.dart';
+import 'package:game_islami/app/modules/sambung_ayat/bindings/sambung_ayat_binding.dart';
+import 'package:game_islami/app/modules/sambung_ayat/views/sambung_ayat_view.dart';
+import 'package:game_islami/app/modules/sambung_ayat_game/bindings/sambung_ayat_game_binding.dart';
+import 'package:game_islami/app/modules/sambung_ayat_game/views/sambung_ayat_game_view.dart';
+import 'package:game_islami/app/modules/tebak_surah/bindings/tebak_surah_binding.dart';
+import 'package:game_islami/app/modules/tebak_surah/views/tebak_surah_view.dart';
+import 'package:game_islami/app/modules/tebak_surah_game/bindings/tebak_surah_game_binding.dart';
+import 'package:game_islami/app/modules/tebak_surah_game/views/tebak_surah_game_view.dart';
 
 part 'app_routes.dart';
 
@@ -35,6 +45,31 @@ class AppPages {
       binding: RankBinding(),
       transition: Transition.fadeIn,
       transitionDuration: Duration(milliseconds: 300),
+    ),
+    GetPage(
+      name: _Paths.SAMBUNG_AYAT,
+      page: () => SambungAyatView(),
+      binding: SambungAyatBinding(),
+    ),
+    GetPage(
+      name: _Paths.TEBAK_SURAH,
+      page: () => TebakSurahView(),
+      binding: TebakSurahBinding(),
+    ),
+    GetPage(
+      name: _Paths.SAMBUNG_AYAT_GAME,
+      page: () => SambungAyatGameView(),
+      binding: SambungAyatGameBinding(),
+    ),
+    GetPage(
+      name: _Paths.PILIH_SURAH,
+      page: () => PilihSurahView(),
+      binding: PilihSurahBinding(),
+    ),
+    GetPage(
+      name: _Paths.TEBAK_SURAH_GAME,
+      page: () => TebakSurahGameView(),
+      binding: TebakSurahGameBinding(),
     ),
   ];
 }
--- lib/app/routes/app_routes.dart
+++ lib/app/routes/app_routes.dart
@@ -7,10 +7,20 @@ abstract class Routes {
   static const HOME = _Paths.HOME;
   static const PROFILE = _Paths.PROFILE;
   static const RANK = _Paths.RANK;
+  static const SAMBUNG_AYAT = _Paths.SAMBUNG_AYAT;
+  static const TEBAK_SURAH = _Paths.TEBAK_SURAH;
+  static const SAMBUNG_AYAT_GAME = _Paths.SAMBUNG_AYAT_GAME;
+  static const PILIH_SURAH = _Paths.PILIH_SURAH;
+  static const TEBAK_SURAH_GAME = _Paths.TEBAK_SURAH_GAME;
 }
 
 abstract class _Paths {
   static const HOME = '/home';
   static const PROFILE = '/profile';
   static const RANK = '/rank';
+  static const SAMBUNG_AYAT = '/sambung-ayat';
+  static const TEBAK_SURAH = '/tebak-surah';
+  static const SAMBUNG_AYAT_GAME = '/sambung-ayat-game';
+  static const PILIH_SURAH = '/pilih-surah';
+  static const TEBAK_SURAH_GAME = '/tebak-surah-game';
 }
