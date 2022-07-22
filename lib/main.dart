e6b21ec4754c2bb673110c48c34c17fe475e3ec6
mrezkys <mrezkysulihin@gmail.com>
2022-07-22 17:11:55 +0800

    feat: add firebase configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,57 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:game_islami/app/controllers/loading_controller.dart';
 import 'package:game_islami/app/controllers/main_bottom_navigation_bar_controller.dart';
+import 'package:game_islami/app/controllers/user_info_controller.dart';
 import 'package:game_islami/app/style/app_color.dart';
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:game_islami/app/widgets/loading_screen.dart';
+import 'firebase_options.dart';
 
 import 'package:get/get.dart';
 
 import 'app/routes/app_pages.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   Get.put(MainBottomNavigationBarController());
   Get.put(LoadingController());
+  Get.put(UserInfoController());
   SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
     statusBarColor: AppColor.secondary,
     statusBarBrightness: Brightness.dark,
   ));
   runApp(
-    GetMaterialApp(
-      title: "Application",
-      initialRoute: AppPages.INITIAL,
-      getPages: AppPages.routes,
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        fontFamily: 'inter',
-        scaffoldBackgroundColor: Colors.white,
-        textTheme: TextTheme().apply(
-          bodyColor: AppColor.secondary,
-          displayColor: AppColor.secondary,
-        ),
-      ),
-    ),
+    StreamBuilder<User?>(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return MaterialApp(
+              home: LoadingScreen(
+                loadingBackgroundColor: AppColor.secondaryLight,
+                circularProgressColor: AppColor.primary,
+              ),
+            );
+          }
+          return GetMaterialApp(
+            title: "Application",
+            initialRoute: snapshot.data != null ? Routes.HOME : Routes.LOGIN,
+            getPages: AppPages.routes,
+            debugShowCheckedModeBanner: false,
+            theme: ThemeData(
+              fontFamily: 'inter',
+              scaffoldBackgroundColor: Colors.white,
+              textTheme: TextTheme().apply(
+                bodyColor: AppColor.secondary,
+                displayColor: AppColor.secondary,
+              ),
+            ),
+          );
+        }),
   );
 }
