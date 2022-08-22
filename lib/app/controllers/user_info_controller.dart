2d3697fa762f4ce13c1f2cde74127b5b77fd50f6
mrezkys <mrezkysulihin@gmail.com>
2022-08-22 14:26:48 +0800

    feat: getUserRank()

--- lib/app/controllers/user_info_controller.dart
+++ lib/app/controllers/user_info_controller.dart
@@ -6,6 +6,25 @@ class UserInfoController extends GetxController {
   FirebaseAuth auth = FirebaseAuth.instance;
   FirebaseFirestore firestore = FirebaseFirestore.instance;
 
+  RxnInt userRank = RxnInt();
+
+  Future<int> getUserRank() async {
+    String currentUserUID = auth.currentUser!.uid;
+    CollectionReference usersCollection = firestore.collection('users');
+    QuerySnapshot<Object?> usersCollectionQuery = await usersCollection.orderBy('point', descending: true).get();
+    List<QueryDocumentSnapshot<Object?>> listUserDoc = usersCollectionQuery.docs;
+    int rank = 0;
+    for (var i = 0; i < listUserDoc.length; i++) {
+      rank += 1;
+      Map<String, dynamic> doc = listUserDoc[i].data() as Map<String, dynamic>;
+      if (currentUserUID == doc['uid']) {
+        break;
+      }
+    }
+    print('rank : $rank');
+    return rank;
+  }
+
   Stream<DocumentSnapshot<Map<String, dynamic>>> streamUser() async* {
     String uid = auth.currentUser!.uid;
     yield* firestore.collection("users").doc(uid).snapshots();
@@ -19,6 +38,11 @@ class UserInfoController extends GetxController {
   @override
   void onReady() {
     super.onReady();
+    if (auth.currentUser != null) {
+      getUserRank().then((value) {
+        userRank.value = value;
+      });
+    }
   }
 
   @override
