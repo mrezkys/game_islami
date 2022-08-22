a9ec518b6b6ad50ac2daf48a7b10461cf7e814be
mrezkys <mrezkysulihin@gmail.com>
2022-08-22 14:29:39 +0800

    feat: rank

--- lib/app/modules/rank/controllers/rank_controller.dart
+++ lib/app/modules/rank/controllers/rank_controller.dart
@@ -1,6 +1,40 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:game_islami/app/data/models/rank.dart';
 import 'package:get/get.dart';
 
 class RankController extends GetxController {
+  FirebaseAuth auth = FirebaseAuth.instance;
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+  Stream<QuerySnapshot<Map<String, dynamic>>> getUserData() async* {
+    yield* firestore.collection('users').orderBy('point', descending: true).snapshots();
+  }
+
+  // i made this because i cant access index from stream
+  getRank(int rank, String? uid, var data) {
+    print('uid1 : $uid ');
+    bool thisUser = false;
+    if (auth.currentUser!.uid == uid) thisUser = true;
+
+    for (int i = 0; i < data.length; i++) {
+      if (uid == data[i].id) {
+        rank = i + 1;
+      }
+    }
+
+    print(data[0].id);
+
+    bool isBig3 = false;
+    if (rank <= 3 && rank > 0) isBig3 = true;
+
+    return Rank(
+      number: rank,
+      isBig3: isBig3,
+      thisUser: thisUser,
+    );
+  }
+
   final count = 0.obs;
   @override
   void onInit() {
--- lib/app/modules/rank/views/rank_view.dart
+++ lib/app/modules/rank/views/rank_view.dart
@@ -1,5 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:game_islami/app/data/models/rank.dart';
 import 'package:game_islami/app/style/app_color.dart';
 import 'package:game_islami/app/widgets/main_bottom_navigation_bar.dart';
 import 'package:game_islami/app/widgets/user_info.dart';
@@ -21,17 +23,39 @@ class RankView extends GetView<RankController> {
             // Section 1 - Header
             UserInfo(),
             // Section 2 - Ranking
-            ListView.separated(
-              shrinkWrap: true,
-              itemCount: 3,
-              padding: EdgeInsets.symmetric(horizontal: 16, vertical: 24),
-              separatorBuilder: (context, index) => SizedBox(height: 16),
-              itemBuilder: (context, index) {
-                return RankTile(
-                  ranking: index + 1,
-                  isBig3: true,
-                  name: "Muhammad Rezky Sulihin",
-                  point: 2000,
+            StreamBuilder<QuerySnapshot>(
+              stream: controller.getUserData(),
+              builder: (context, snapshot) {
+                if (snapshot.hasError) {
+                  return Text('errr');
+                }
+
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return Text('loading');
+                }
+                return ListView(
+                  shrinkWrap: true,
+                  physics: NeverScrollableScrollPhysics(),
+                  padding: EdgeInsets.all(16),
+                  children: snapshot.data!.docs.map((DocumentSnapshot document) {
+                    Map<String, dynamic> userData = document.data()! as Map<String, dynamic>;
+
+                    print('dec : ${userData}');
+
+                    List<QueryDocumentSnapshot<Object?>> listUserData = snapshot.data!.docs;
+
+                    int initRank = 0;
+                    Rank rank = controller.getRank(initRank, userData['uid'], listUserData);
+
+                    return RankTile(
+                      ranking: rank.number,
+                      name: userData['name'],
+                      point: userData['point'],
+                      isBig3: rank.isBig3,
+                      thisUser: rank.thisUser,
+                      margin: EdgeInsets.only(bottom: 16),
+                    );
+                  }).toList(),
                 );
               },
             ),
@@ -47,12 +71,16 @@ class RankTile extends StatelessWidget {
   final String name;
   final int point;
   final bool isBig3;
+  final bool thisUser;
+  final EdgeInsetsGeometry margin;
 
   RankTile({
     required this.ranking,
     required this.name,
     required this.point,
     required this.isBig3,
+    this.thisUser = false,
+    this.margin = EdgeInsets.zero,
   });
 
   @override
@@ -60,8 +88,9 @@ class RankTile extends StatelessWidget {
     return Container(
       width: MediaQuery.of(context).size.width,
       padding: EdgeInsets.all(16),
+      margin: margin,
       decoration: BoxDecoration(
-        color: Colors.white,
+        color: (thisUser) ? AppColor.primary : Colors.white,
         borderRadius: BorderRadius.circular(8),
         border: Border.all(color: AppColor.grey, width: 1),
       ),
